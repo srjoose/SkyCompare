d26848dbcb2848262f48be87efeda55cb9ee40c7
srjoose <91277869+srjoose@users.noreply.github.com>
2025-06-05 21:46:12 +0200

    actualizado cors.go

--- skycompare-backend-main/internal/middleware/cors.go
+++ skycompare-backend-main/internal/middleware/cors.go
@@ -1,4 +1,8 @@
 package middleware
+
+import "net/http"
+
+
 func middlewareCors(next http.Handler) http.Handler {
     return http.HandlerFunc(
         func(w http.ResponseWriter, r *http.Request) {
